abc165d18c35dbf675abb4d5f3000d7584319ab3
Vicky Zhang <vicky11z@berkeley.edu>
2015-01-14 22:52:22 -0800

    First commit

---
+++ fbreader.py
@@ -0,0 +1,22 @@
+class fbConfig():
+	def __init__(self, srcDir, destDirs):
+		self.srcDir = srcDir
+		self.destDirs = destDirs
+
+def readConf(filePath):
+	f = open(filePath, 'r')
+	srcDir, destDirs = "", []
+	for line in f:
+		pair = line.split('=')
+		if pair[0].strip() == 'fb.sourceFolder':
+			srcDir = pair[1].strip()
+		elif pair[0].strip() == 'fb.destinationFolders':
+
+			destDirs = pair[1].strip().split(",")
+	f.close()
+	return fbConfig(srcDir, destDirs)
+
+
+confObj = readConf('fb.conf')
+print('Src =', confObj.srcDir)
+print('Dest =', confObj.destDirs)
